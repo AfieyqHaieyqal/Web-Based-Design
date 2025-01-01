97ada679cabad6d88781bb1e52ff093a7075fed1
AfieyqHaieyqal <139975730+AfieyqHaieyqal@users.noreply.github.com>
2025-01-02 04:46:08 +0800

    Add files via upload

---
+++ lab 8/exercise/js/scripts.js
@@ -0,0 +1,16 @@
+/*!
+* Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
